4399bac20d06c0f3469f9e93e5834db6119dbb67
zaini01 <o.z41n1@gmail.com>
2021-01-03 22:29:50 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const routs = require('./routs/index')
 
 app.set('view engine','ejs')
